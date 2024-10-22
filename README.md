df78d1c77221540ac4d0d01a027bd8fbf6246d8b
MUHAMMAD-SARIM <muhammad.sarim001100@gmail.com>
2024-10-22 15:06:44 +0500

    Updated TypeScript code with new features

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Cli-Calculator
+# cli-calculaptr
---
+++ index.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const Num = await inquirer.prompt([
+    { message: " Enter first number", type: "number", name: "firstNumber" },
+    { message: " Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of these operator to perform operation",
+        name: "operation",
+        type: "list",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (Num.operation === "Addition") {
+    console.log(Num.firstNumber + Num.secondNumber);
+}
+else if (Num.operation === "Subtraction") {
+    console.log(Num.firstNumber - Num.secondNumber);
+}
+else if (Num.operation === "Multiplication") {
+    console.log(Num.firstNumber * Num.secondNumber);
+}
+else if (Num.operation === "Division") {
+    console.log(Num.firstNumber / Num.secondNumber);
+    console.log("please select valid operator");
+}
